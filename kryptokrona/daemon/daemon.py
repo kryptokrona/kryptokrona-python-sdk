eda24590dce007b405d619e73f0bdcef59d2ca1a
Marcus Cvjeticanin <mjovanc@protonmail.com>
2022-07-05 20:03:34 +0200

    Updating daemon

--- kryptokrona/daemon/daemon.py
+++ kryptokrona/daemon/daemon.py
@@ -1,4 +1,8 @@
 class Daemon:
+    """Daemon definition."""
 
-    def __init__(self, hostname):
+    def __init__(self, hostname, port, ssl=False, use_raw_blocks=False):
         self.hostname = hostname
+        self.port = port
+        self.ssl = ssl
+        self.use_raw_blocks = use_raw_blocks
